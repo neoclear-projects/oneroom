4995c46c2dd0ef19af785ca15688f536357fa264
XGsombra <xuduo.gu@mail.utoronto.ca>
2021-04-28 15:26:50 -0400

    Added start code for backend

---
+++ backend/oneroom/src/main/java/tiafs/oneroom/OneroomApplication.java
@@ -0,0 +1,13 @@
+package tiafs.oneroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneroomApplication.class, args);
+	}
+
+}
---
+++ backend/oneroom/src/test/java/tiafs/oneroom/OneroomApplicationTests.java
@@ -0,0 +1,13 @@
+package tiafs.oneroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
